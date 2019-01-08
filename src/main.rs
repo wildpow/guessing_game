d741d43fd54d477f74932e22453223af228024c5
wildpow <element1028@gmail.com>
2019-01-07 17:43:07 -0800

    Finished chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guest.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guest.");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        // mutable var
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        // shadowing
+        let guess: u32 = guess.trim().parse().expect("please type a number");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 
 }
